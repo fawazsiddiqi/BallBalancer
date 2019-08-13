e0f75dc9e7deeb98c1286ffe8cac301f2be8df66
Fawaz Siddiqi <mfawazsiddiqi@gmail.com>
2019-08-13 10:35:34 +0400

    You do not need this file, it is bsing used got GitHub to detected project language

---
+++ LanguageFile.cs
@@ -0,0 +1,14 @@
+
+using System;
+namespace LangaugeDetect
+{
+    class Lang 
+    {
+        static void Main() 
+        {
+            Console.WriteLine("You do not need this file, it is bsing used got GitHub to detected project language");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
